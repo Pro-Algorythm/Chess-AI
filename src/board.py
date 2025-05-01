2a41fc25977546d95de60b1b8af2546429500ab0
Pro-Algorythm <aicon51214@gmail.com>
2025-05-02 00:19:28 +0300

    Completed AI
    Implemented changes::
     - Improved evaluation (takes space into account)
     - Added CLI playing functionality
     - Currently adding GUI

--- src/board.py
+++ src/board.py
@@ -46,7 +46,10 @@ class Board():
         self.board = board
          
     def get_all_actions(self, side):
+        # Initialize moves array
         moves = []
+
+        # Loop through the whole board storing all actions of a side
         for i in self.board:
             for j in i:
                 if j != None:
@@ -54,6 +57,7 @@ class Board():
                         actions = j.get_actions(self)
                         if len(actions) != 0:
                             moves.extend(actions)
+
         return moves
 
     def is_terminal(self):
@@ -70,22 +74,90 @@ class Board():
 
         # Check for all results
         if white_actions == 0 and not white_king.check:
-            return 'stalemate'
+            return 0
         elif white_actions == 0 and white_king.check:
-            return 'white'
+            return float('inf')
         elif black_actions == 0 and not black_king.check:
-            return 'stalemate'
+            return 0
         elif black_actions == 0 and black_king.check:
-            return 'white'
+            return float('-inf')
         else:
             return False
 
     def get_util(self):
-        eval = self.get_material(side = 'w') - self.get_material(side = 'b')
+        # Get the material of both sides
+        white_material = self.get_material(side = 'w')
+        black_material = self.get_material(side = 'b')
+
+        # Caclulate their space/control in enemys side
+        white_actions = self.get_all_actions(side = 'w')
+        black_actions = self.get_all_actions(side = 'b')
+
+        white_space = 0
+        for action in white_actions:
+            if action.end_pos[0] > 3:
+                white_space += 1
+        black_space = 0
+        for action in black_actions:
+            if action.end_pos[0] < 4:
+                black_space += 1
+
+        # Get the evaluation and return it
+        eval = (white_material + white_space*0.5) - (black_material - black_space*0.5)
         return eval
     
     def get_material(self, side):
-        # TODO: Return the value of the material for the specified side
+        pawn_pst = [
+            [0 for i in range(8)],
+            [0, 0, -0.5, -1, -1, -0.5, 0, 0],
+            [0.5, 0.5, 0.75, 0.75, 0.75, 0.75, 0.5, 0.5],
+            [0.5, 0.5, 1, 2, 2, 1, 0.5, 0.5],
+            [0.5, 1, 2, 2, 2, 2, 1, 0.5],
+            [1, 1, 2.25, 2.25, 2.25, 2.25, 1, 1],
+            [2, 2, 2.75, 2.75, 2.75, 2.75, 2, 2],
+            [10 for i in range(8)]
+        ]
+
+        knight_pst = [
+            [0 for i in range(8)],
+            [-1, -0.5, 0.75, 0.75, 0.75, 0.75, -0.5, -1],
+            [-1.25, 1.5, 1.75, 1.75, 1.75, 1.75, 1.5, -1.25],
+            [-2.25, 2.5, 5, 5, 5, 5, 2.5, -2.25],
+            [-2.25, 2.5, 5, 5, 5, 5, 2.5, -2.25],
+            [-1.25, 1.5, 1.75, 1.75, 1.75, 1.75, 1.5, -1.25],
+            [-1, 0.5, 0.75, 0.75, 0.75, 0.75, 0.5, -1],
+            [-1, 0.5, 0.75, 0.75, 0.75, 0.75, 0.5, -1],
+        ]
+
+        bishop_pst = [
+            [-1 for i in range(8)],
+            [-0.5, 3, 1, 1, 1, 1, 3, -0.5],
+            [-0.5, 1, 3, 1, 1, 3, 1, -0.5],
+            [-0.5, 1, 3, 1, 1, 3, 1, -0.5],
+            [-0.5, 1, 3, 1, 1, 3, 1, -0.5],
+            [-0.5, 1, 3, 1, 1, 3, 1, -0.5],
+            [-0.5, 1, 3, 1, 1, 3, 1, -0.5],
+            [-1 for i in range(8)],
+        ]
+
+        king_pst = [
+            [3, 3, 3, 5, -2, 3, 5, 3],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [-1 for i in range(8)],
+            [-1 for i in range(8)],
+            [-1 for i in range(8)],
+            [-1 for i in range(8)],
+            [-1 for i in range(8)],
+            [0 for i in range(8)]
+        ]
+
+        # Flip the PST's for black
+        if side == 'b':
+            king_pst = king_pst[::-1]
+            bishop_pst = bishop_pst[::-1]
+            knight_pst = knight_pst[::-1]
+            pawn_pst = pawn_pst[::-1]
+
         material = 0
         for row in self.board:
             for sq in row:
@@ -93,8 +165,9 @@ class Board():
                     if sq.side == side:
                         if isinstance(sq, Queen): material+=9
                         if isinstance(sq, Rook): material+=5
-                        if isinstance(sq, Knight) or isinstance(sq, Bishop): material+=3
-                        if isinstance(sq, Pawn): material+=1
+                        if isinstance(sq, Knight): material+= 3 + knight_pst[sq.pos[0]][sq.pos[1]]
+                        if isinstance(sq, Bishop): material+= 3 + bishop_pst[sq.pos[0]][sq.pos[1]]
+                        if isinstance(sq, Pawn): material+= 1 + pawn_pst[sq.pos[0]][sq.pos[1]]
         return material
 
     def __str__(self):
--- src/main.py
+++ src/main.py
@@ -1,3 +1,91 @@
-from pieces import *
 from board import *
-import copy
+from argparse import ArgumentParser
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('-u', '--gui', action = 'store_true')
+    args = parser.parse_args()
+    gui = args.gui
+
+    board = Board()
+    if not gui:
+        player = input("Play as black (b) or white (w): ")
+    else:
+        pass
+    ai = 'w' if player == 'b' else 'b'
+    turn = 'w'
+
+    while True:
+        terminality = board.is_terminal()
+        if terminality:
+            if terminality == player:
+                if not gui:
+                    print("You won.")
+                    exit()
+                else:
+                    pass
+            elif terminality == ai:
+                if not gui:
+                    print("You lost, the AI won.")
+                    exit()
+                else:
+                    pass
+            else:
+                if not gui:
+                    print("Its a draw by stalemate.")
+                    exit()
+                else:
+                    pass
+        if player == turn:
+            if not gui:
+                print(board)
+                move = input('Enter move as (peice end_pos promotion(if applicable) castling(if applicable)): ')
+                peice, end_pos, promotion, castling = move.split()
+                if promotion == 'None':
+                    promotion = None
+                if castling == 'None':
+                    castling = None
+                peice = board.board[int(peice[1])][int(peice[3])]
+                move = Move(peice, peice.pos, (int(end_pos[1]), int(end_pos[3])), promoted_peice = promotion, castling = castling)
+            else:
+                pass
+
+        else:
+            global depth
+            depth = 10
+            move = get_best_move(board, turn)[0]
+        
+        # Make the move and update the turn
+        move.peice.move(move, board)
+        turn = 'w' if turn == 'b' else 'b'
+
+
+def get_best_move(board, turn):
+    global depth
+    depth -= 1
+    actions = board.get_all_actions(side = turn)
+    
+    best_move = None
+    for action in actions:
+        dummy = deepcopy(board)
+        peice = dummy.board[action.start_pos[0]][action.start_pos[1]]
+        peice.move(action, dummy)
+        terminality = dummy.is_terminal()
+        eval = dummy.get_util()
+        if terminality != False:
+            if terminality == (float('inf') if turn == 'w' else float('-inf')):
+                return (action, terminality)
+            else:
+                if best_move == None or ((best_move[1] < 0 and turn == 'b') or (best_move[1] > 0 and turn == 'w')):
+                    best_move = (action, terminality)
+        if not depth <= 0:
+            move = get_best_move(dummy, turn = 'w' if turn == 'b' else 'b')
+        else:
+            move = (action, eval)
+
+        if best_move == None or ((eval > best_move[1] and turn == 'w') or (eval < best_move[1] and turn == 'b')):
+            best_move = move
+    return best_move
+        
+if __name__ == '__main__':
+    main()
